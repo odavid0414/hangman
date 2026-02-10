463ae4968b611a4e81224bd1e4fac4a7018f3c29
David Olah <59338719+odavid0414@users.noreply.github.com>
2026-02-10 22:17:06 +0100

    firebase connection and uploading initial words

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -1,12 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDY19E_3Uv8GugzxOwb6o9ONMuCyB2jZig",
   authDomain: "hangman-db-fc4a9.firebaseapp.com",
@@ -17,8 +10,6 @@ const firebaseConfig = {
   measurementId: "G-M3XB4K499C"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
